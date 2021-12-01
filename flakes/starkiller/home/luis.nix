a871bf9e235c36ac9555e6b27f769ff5fb52f127
Luis De Siqueira <luis@pleme.io>
2021-12-01 09:47:12 +0000

    prezto enable

--- flakes/starkiller/home/luis.nix
+++ flakes/starkiller/home/luis.nix
@@ -1,22 +1,30 @@
 { config, pkgs, ... }: { 
   programs.home-manager.enable = true;
   
-  # alacritty
+  # starship command prompt
+  programs.starship.enable = true;
+
+  # alacritty terminal
   programs.alacritty.enable = true;
 
-  # zsh
+  # zsh shell
   programs.zsh.enable = true;
   programs.zsh.enableAutosuggestions = true;
   programs.zsh.enableCompletion = true;
   programs.zsh.prezto.enable = true;
 
+  # home configuration
   home.username = "luis"; 
   home.homeDirectory = "/home/luis"; 
+
+  # user packages
   home.packages = with pkgs; [
     jq
     neovim
     ripgrep
   ];
+  
+  # session variables
   home.sessionVariables = {
     EDITOR = "neovim";
   };
