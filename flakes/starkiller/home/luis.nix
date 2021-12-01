dad4586d7c4568611161ebb08220a1044ce10696
Luis De Siqueira <luis@pleme.io>
2021-12-01 21:14:45 +0000

    prezto enable

--- flakes/starkiller/home/luis.nix
+++ flakes/starkiller/home/luis.nix
@@ -1,4 +1,11 @@
 { config, pkgs, ... }: { 
+  # nixpkgs overlay for neovim
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+
   # home-manager manage itself
   programs.home-manager.enable = true;
 
