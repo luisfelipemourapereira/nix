c6087499385ba2ce2ef22b3311cd2d763da1ff2f
Luis De Siqueira <luis@pleme.io>
2021-12-01 10:01:20 +0000

    prezto enable

--- flakes/starkiller/home/luis.nix
+++ flakes/starkiller/home/luis.nix
@@ -23,6 +23,9 @@
   programs.neovim.viAlias = true;
   programs.neovim.vimAlias = true;
   programs.neovim.vimdiffAlias = true;
+  programs.neovim.withNodeJs = true;
+  programs.neovim.withRuby = true;
+  programs.neovim.withPython3 = true;
 
   # starship command prompt
   programs.starship.enable = true;
