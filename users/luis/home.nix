029caf2a4cd5e942933b0e75a16e24dbf45e8f74
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-10 19:18:06 -0800

    [vader]

--- users/luis/home.nix
+++ users/luis/home.nix
@@ -28,22 +28,16 @@
   # starship command prompt
   programs.starship.enable = true;
   programs.starship.enableZshIntegration = true;
-  programs.starship.settings = ''
-    {
-      add_newline = false;
-      format = lib.concatStrings [
-        "$line_break"
-        "$package"
-        "$line_break"
-        "$character"
-      ];
-      scan_timeout = 10;
-      character = {
-        success_symbol = "➜";
-        error_symbol = "➜";
-      };
-    }
-  '';
+  programs.starship.settings = {
+    add_newline = false;
+    format =
+      lib.concatStrings [ "$line_break" "$package" "$line_break" "$character" ];
+    scan_timeout = 10;
+    character = {
+      success_symbol = "➜";
+      error_symbol = "➜";
+    };
+  };
 
   # alacritty terminal
   # programs.alacritty.enable = true;
