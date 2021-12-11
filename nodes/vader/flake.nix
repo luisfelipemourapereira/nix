a276a0fd70e24dbe5f439aa66fd4eabe9f65189d
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-10 16:22:23 -0800

    [vader]

--- nodes/vader/flake.nix
+++ nodes/vader/flake.nix
@@ -3,6 +3,7 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.users.path = "../users/luis";
 
   outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
@@ -10,12 +11,12 @@
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.luis = import ../users/luis/home.nix;
-          }
+          # home-manager.nixosModules.home-manager
+          # {
+          # home-manager.useGlobalPkgs = true;
+          # home-manager.useUserPackages = true;
+          # home-manager.users.luis = import ../users/luis/home.nix;
+          # }
         ];
       };
     };
