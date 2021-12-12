49494719e359e4ac3d15e8f3d0a83d2a70820dca
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-11 16:59:02 -0800

    [init]

--- users/luis/neovim.nix
+++ users/luis/neovim.nix
@@ -1,11 +1,11 @@
 { config, pkgs, nixpkgs, lib, programs, ... }:
-with programs.neovim; {
-  enable = true;
+with programs; {
+  neovim.enable = true;
   # package = pkgs.neovim-nightly;
-  viAlias = true;
-  vimAlias = true;
-  vimdiffAlias = true;
-  withNodeJs = true;
-  withRuby = true;
-  withPython3 = true;
+  neovim.viAlias = true;
+  neovim.vimAlias = true;
+  neovim.vimdiffAlias = true;
+  neovim.withNodeJs = true;
+  neovim.withRuby = true;
+  neovim.withPython3 = true;
 }
