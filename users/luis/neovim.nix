6f7e1d6fb9244948fa12aa5523099f855bf5e7af
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-11 16:43:01 -0800

    [init]

--- users/luis/neovim.nix
+++ users/luis/neovim.nix
@@ -1,22 +1,11 @@
 { config, pkgs, nixpkgs, lib, ... }:
 with programs.neovim; {
-  # with programs.neovim;
-  # {
   enable = true;
+  # package = pkgs.neovim-nightly;
   viAlias = true;
   vimAlias = true;
   vimdiffAlias = true;
   withNodeJs = true;
   withRuby = true;
   withPython3 = true;
-  enable = true;
-  # }
-  # # uncomment to target nightly build
-  # # programs.neovim.package = pkgs.neovim-nightly;
-  # programs.neovim.viAlias = true;
-  # programs.neovim.vimAlias = true;
-  # programs.neovim.vimdiffAlias = true;
-  # programs.neovim.withNodeJs = true;
-  # programs.neovim.withRuby = true;
-  # programs.neovim.withPython3 = true;
 }
