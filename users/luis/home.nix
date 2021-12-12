2f3f218218b7d11fc2c78e18141060e3eab3bfa6
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-11 16:57:14 -0800

    [init]

--- users/luis/home.nix
+++ users/luis/home.nix
@@ -1,6 +1,7 @@
 { config, pkgs, nixpkgs, lib, ... }: {
-  imports = [ ./packages.nix ./environment.nix ./zsh.nix ./starship.nix ];
+  # imports = [ ./packages.nix ./environment.nix ./zsh.nix ./starship.nix ];
   # imports = [ ./packages.nix ./environment.nix ./zsh.nix ];
+  imports = [ ./packages.nix ./environment.nix ./neovim.nix ];
 
   # home-manager manage itself
   programs.home-manager.enable = true;
