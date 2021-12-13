f23695a55f1653e9653cd4c61e2429143cbdcb34
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-12 17:01:59 -0800

    [init]

--- users/luis/neovim.nix
+++ users/luis/neovim.nix
@@ -1,10 +1,9 @@
-{ config, lib, specialArgs, options, modulesPath, nixosConfig, osConfig }:
-{
-  # programs.neovim.enable = true;
-  # programs.neovim.viAlias = true;
-  # programs.neovim.vimAlias = true;
-  # programs.neovim.vimdiffAlias = true;
-  # programs.neovim.withNodeJs = true;
-  # programs.neovim.withRuby = true;
-  # programs.neovim.withPython3 = true;
+{ lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig }: {
+  programs.neovim.enable = true;
+  programs.neovim.viAlias = true;
+  programs.neovim.vimAlias = true;
+  programs.neovim.vimdiffAlias = true;
+  programs.neovim.withNodeJs = true;
+  programs.neovim.withRuby = true;
+  programs.neovim.withPython3 = true;
 }
