8e19344e0e1046feef39c50dffb55f134304d2d9
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-12 17:06:45 -0800

    [init]

--- users/luis/home.nix
+++ users/luis/home.nix
@@ -1,9 +1,6 @@
 { config, pkgs, nixpkgs, lib, specialArgs, options, modulesPath, nixosConfig
 , osConfig }: {
   programs.home-manager.enable = true;
-  # imports = [ ./packages.nix ./environment.nix ./zsh.nix ./starship.nix ];
-  # imports = [ ./packages.nix ./environment.nix ./zsh.nix ];
-  # imports = [ ./packages.nix ./environment.nix ./neovim.nix ];
   imports = [
     ./packages.nix
     ./environment.nix
@@ -11,9 +8,7 @@
     ./shell.nix
     ./neovim.nix
   ];
-
   home.username = "luis";
   home.homeDirectory = "/home/luis";
-
   home.stateVersion = "21.11";
 }
