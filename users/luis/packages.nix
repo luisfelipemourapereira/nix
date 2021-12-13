afa06c558e418df26dbd1798c0b6ec758cb86f41
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-12 17:01:10 -0800

    [init]

--- users/luis/packages.nix
+++ users/luis/packages.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig }: {
+{ pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig }: {
   home.packages = with pkgs; [ ripgrep ];
   # jq
   programs.jq.enable = true;
