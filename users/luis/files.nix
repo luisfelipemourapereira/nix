b89400659ace1bf9be7a86c35ca64f300a3e45b7
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 20:11:47 -0800

    [init]

--- users/luis/files.nix
+++ users/luis/files.nix
@@ -4,6 +4,7 @@
     "code/self/ltk".source = builtins.fetchGit {
       url = "ssh://git@github.com/luisfelipemourapereira/ltk.git";
       ref = "master";
+      rev = "1173bd1";
       # owner = "k3rni";
       # repo = "foggy";
       # rev = "fd76b28";
@@ -20,7 +21,6 @@
     # # sha256 = "0lfm7kczgdlzfcc14qj8539y679lf5qcydz0xv72szn7h9wzaaiz";
     # owner = "luisfelipemourapereira";
     # repo = "ltk";
-    # rev = "1173bd1";
     # sha256 =
     # "d2428752bf2d2c530b520f3401dfbb26805cbbd7184d3bc1448562db17d43922";
     # };
