819f7ecee6e1948dc4e2a06c1f374ca193fb8788
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 20:31:50 -0800

    [init]

--- users/luis/shell.nix
+++ users/luis/shell.nix
@@ -1,12 +1,12 @@
 { lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig }: {
   programs.zsh.enable = true;
   programs.zsh.prezto.enable = true;
+  programs.zsh.dotDir = ".config/zsh";
   programs.zsh.enableAutosuggestions = true;
   programs.zsh.enableCompletion = true;
   programs.zsh.enableSyntaxHighlighting = true;
   programs.zsh.defaultKeymap = "viins";
   programs.zsh.autocd = false;
-  # programs.zsh.dotDir = ".config/zsh";
   programs.zsh.history.size = 10000000;
   programs.zsh.history.save = 10000000;
 }
