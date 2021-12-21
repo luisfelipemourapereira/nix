27e75c1de4acb95e471594708beb8627105f40ac
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 19:56:28 -0800

    [init]

--- users/luis/files.nix
+++ users/luis/files.nix
@@ -5,7 +5,7 @@
       owner = "luisfelipemourapereira";
       repo = "ltk";
       rev = "master";
-      sha256 = "whateva";
+      sha256 = "d2428752bf2d2c530b520f3401dfbb26805cbbd7184d3bc1448562db17d43922";
     };
   };
 }
