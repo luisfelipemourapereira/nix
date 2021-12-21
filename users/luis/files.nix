6c1fb66af439993a9fdcded87ed7b5a2cc1f65d1
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 20:15:26 -0800

    [init]

--- users/luis/files.nix
+++ users/luis/files.nix
@@ -5,24 +5,6 @@
       url = "ssh://git@github.com/luisfelipemourapereira/ltk.git";
       ref = "master";
       rev = "6ed0f0328a68f38b5ce099156121bcf09cd762cd";
-      # owner = "k3rni";
-      # repo = "foggy";
-      # rev = "fd76b28";
-      # sha256 = "0lfm7kczgdlzfcc14qj8539y679lf5qcydz0xv72szn7h9wzaaiz";
-      # owner = "luisfelipemourapereira";
-      # repo = "ltk";
-      # sha256 =
-      # "d2428752bf2d2c530b520f3401dfbb26805cbbd7184d3bc1448562db17d43922";
     };
-    # "code/self/ltk".source = pkgs.fetchFromGitHub {
-    # # owner = "k3rni";
-    # # repo = "foggy";
-    # # rev = "fd76b28";
-    # # sha256 = "0lfm7kczgdlzfcc14qj8539y679lf5qcydz0xv72szn7h9wzaaiz";
-    # owner = "luisfelipemourapereira";
-    # repo = "ltk";
-    # sha256 =
-    # "d2428752bf2d2c530b520f3401dfbb26805cbbd7184d3bc1448562db17d43922";
-    # };
   };
 }
--- users/luis/neovim.nix
+++ users/luis/neovim.nix
@@ -1,12 +1,12 @@
-{ pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig
-}: {
-  programs.neovim.enable = true;
-  programs.neovim.viAlias = true;
-  programs.neovim.vimAlias = true;
-  programs.neovim.vimdiffAlias = true;
-  programs.neovim.withNodeJs = true;
-  programs.neovim.withRuby = true;
-  programs.neovim.withPython3 = true;
+{ pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig }:
+{
+  # programs.neovim.enable = true;
+  # programs.neovim.viAlias = true;
+  # programs.neovim.vimAlias = true;
+  # programs.neovim.vimdiffAlias = true;
+  # programs.neovim.withNodeJs = true;
+  # programs.neovim.withRuby = true;
+  # programs.neovim.withPython3 = true;
   # programs.neovim.extraPackages = [ pkgs.shfmt ];
 
   # programs.neovim.plugins = [
