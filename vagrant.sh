dc50733399a2727937c32292c8fc456083378c57
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 18:39:28 -0800

    [init]

--- vagrant.sh
+++ vagrant.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 # build it with nix
-sudo nix-channel --add https://channels.nixos.org/nixos-20.09 nixos
+sudo nix-channel --add https://channels.nixos.org/nixos-unstable
+sudo nix-channel --add https://channels.nixos.org/nixpkgs-unstable
 [ -f custom-configuration.nix ] && 
   sudo cp custom-configuration.nix /etc/nixos/custom-configuration.nix
 sudo nixos-rebuild switch
