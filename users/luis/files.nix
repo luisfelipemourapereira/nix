d6f9f61568e40fbd784a66286767235245870883
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 20:02:18 -0800

    [init]

--- users/luis/files.nix
+++ users/luis/files.nix
@@ -2,15 +2,15 @@
 }: {
   home.file = {
     "code/self/ltk".source = pkgs.fetchFromGitHub {
-      owner = "k3rni";
-      repo = "foggy";
-      rev = "fd76b28";
-      sha256 = "0lfm7kczgdlzfcc14qj8539y679lf5qcydz0xv72szn7h9wzaaiz";
-      # owner = "luisfelipemourapereira";
-      # repo = "ltk";
-      # rev = "master";
-      # sha256 =
-      # "d2428752bf2d2c530b520f3401dfbb26805cbbd7184d3bc1448562db17d43922";
+      # owner = "k3rni";
+      # repo = "foggy";
+      # rev = "fd76b28";
+      # sha256 = "0lfm7kczgdlzfcc14qj8539y679lf5qcydz0xv72szn7h9wzaaiz";
+      owner = "luisfelipemourapereira";
+      repo = "ltk";
+      rev = "1173bd1";
+      sha256 =
+        "d2428752bf2d2c530b520f3401dfbb26805cbbd7184d3bc1448562db17d43922";
     };
   };
 }
