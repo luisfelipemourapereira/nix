14bd811027f0786797b94b6b1266f7592bf508ab
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 18:12:01 -0800

    [init]

--- vagrant.sh
+++ vagrant.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # build it with nix
+sudo nix-channel --add https://channels.nixos.org/nixos-20.09 nixos
 [ -f custom-configuration.nix ] && 
   sudo cp custom-configuration.nix /etc/nixos/custom-configuration.nix
 sudo nixos-rebuild switch
