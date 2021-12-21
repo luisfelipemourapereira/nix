527d35429972b3c75b0f7d6ef1e5c8e815b928ef
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 20:57:17 -0800

    [init]

--- users/luis/files.nix
+++ users/luis/files.nix
@@ -7,7 +7,7 @@
       rev = "6ed0f0328a68f38b5ce099156121bcf09cd762cd";
     };
     "code/pub/consul".source = builtins.fetchGit {
-      url = "ssh://git@github.com/hashicorp/consul.git";
+      url = "ssh://git@github.com/hashicorp/consul";
       ref = "master";
       rev = "09cc3338d4807b5777486aea3ff4d95431ab3abf";
     };
