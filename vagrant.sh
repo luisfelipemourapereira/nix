351ec341f01d6fd4c3e6bd91da0c314bc8d6a7f4
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 19:14:01 -0800

    [init]

--- vagrant.sh
+++ vagrant.sh
@@ -2,7 +2,8 @@
 # build it with nix
 sudo nix-channel --add https://channels.nixos.org/nixos-unstable
 sudo nix-channel --add https://channels.nixos.org/nixpkgs-unstable
+sudo nixos-rebuild switch --upgrade
 [ -f custom-configuration.nix ] && 
   sudo cp custom-configuration.nix /etc/nixos/custom-configuration.nix
-sudo nixos-rebuild switch
-# sudo /vagrant/scripts/enforce/vader.sh
+sudo nixos-rebuild switch --upgrade
+sudo /vagrant/scripts/enforce/vader.sh
--- vagrantbox/essius/nix/configuration.nix
+++ vagrantbox/essius/nix/configuration.nix
@@ -1,5 +1,6 @@
 # nix configuration for essius machine
 { confg, pkgs, ... }: {
+  users.users.vagrant.isNormalUser = true;
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes
