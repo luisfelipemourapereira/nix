9c18558ca2ebae49f7ae8aa37dc8c1fb2c3b3a44
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-20 19:55:29 -0800

    [init]

--- users/luis/files.nix
+++ users/luis/files.nix
@@ -1,6 +1,6 @@
 { lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig }: {
   home.files = {
-    "code/self/ltk".source = lib.fetchFromGitHub {
+    "code/self/ltk".source = pkgs.fetchFromGitHub {
       owner = "luisfelipemourapereira";
       repo = "ltk";
       rev = "master";
