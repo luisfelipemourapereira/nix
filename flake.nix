c3a305725c9ef451447b788bf9c620571c6d67de
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-23 14:06:45 -0800

    remove neovim from luis user

--- flake.nix
+++ flake.nix
@@ -8,6 +8,18 @@
 
   outputs = { nixpkgs, home-manager, self }: {
     nixosConfigurations = {
+      starkiller = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./starkiller/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.luis = import ./users/luis/home.nix;
+          }
+        ];
+      };
       vader = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
