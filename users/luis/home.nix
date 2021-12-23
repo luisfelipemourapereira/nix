e92d31b976451e051d62707af137e5b26788ff22
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-23 12:00:49 -0800

    remove neovim from luis user

--- users/luis/home.nix
+++ users/luis/home.nix
@@ -1,14 +1,9 @@
 { config, pkgs, nixpkgs, lib, specialArgs, options, modulesPath, nixosConfig
 , osConfig }: {
   programs.home-manager.enable = true;
-  imports = [
-    ./packages.nix
-    ./environment.nix
-    ./starship.nix
-    ./shell.nix
-    ./neovim.nix
-    ./files.nix
-  ];
+  imports =
+    [ ./packages.nix ./environment.nix ./starship.nix ./shell.nix ./files.nix ];
+  # ./neovim.nix
   home.username = "luis";
   home.homeDirectory = "/home/luis";
   home.stateVersion = "21.11";
