c87bdbb1bbbf8fcfec8f04abfce1d5fc58214188
luisdesiqueira <luisfelipedesiqueira11@gmail.com>
2021-12-23 17:37:45 -0800

    remove neovim from luis user

--- users/luis/packages.nix
+++ users/luis/packages.nix
@@ -1,6 +1,15 @@
 { pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig
 }: {
-  home.packages = with pkgs; [ ripgrep xsel ];
+  home.packages = with pkgs; [
+    ripgrep
+    xsel
+    {
+      name = "neovim";
+      builder = "${bash}/bin/bash";
+      args = [ ./builder.sh ];
+      system = builtins.currentSystem;
+    }
+  ];
   # jq
   programs.jq.enable = true;
 }
