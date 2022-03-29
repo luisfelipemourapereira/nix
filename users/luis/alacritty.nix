cbbcd117556a87075fe944b64fd2263f7492b2d5
luis de siqueira <luis@pleme.io>
2022-03-29 21:49:04 +0000

    stuff

--- users/luis/alacritty.nix
+++ users/luis/alacritty.nix
@@ -1,4 +1,4 @@
 { pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig}: {
   programs.alacritty.enable = true;
-  xdg.configFile."alacritty/alacritty.yml".source = ../../configurations/alacritty/alacritty.yml;
+  xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
 }
