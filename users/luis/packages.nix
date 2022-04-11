68d0a7aa9c37b7879f8cd7fb77ccc62dea0f970f
luis de siqueira <luis@pleme.io>
2022-04-11 02:39:45 +0000

    fzf

--- users/luis/packages.nix
+++ users/luis/packages.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig
 }: {
-  home.packages = with pkgs; [ ripgrep xsel ];
+  home.packages = with pkgs; [ ripgrep xsel fzf ];
   # jq
   programs.jq.enable = true;
 }
