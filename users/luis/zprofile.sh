378ed6c3a8df317ba9973e6a0dba030bebfd5fe3
luis de siqueira <luis@pleme.io>
2022-04-13 15:07:55 -0700

    add tmux

--- users/luis/zprofile.sh
+++ users/luis/zprofile.sh
@@ -4,7 +4,7 @@ if [ -n "${commands[fzf - share]}" ]; then
 	source "$(fzf-share)/completion.zsh"
 fi
 if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
-	[ -z "$TMUX_PROJECT" ] && export $TMUX_PROJECT=main
+	[ -z "$TMUX_PROJECT" ] && export TMUX_PROJECT=main
 	tmux attach -t $TMUX_PROJECT || tmux new -s $TMUX_PROJECT
 fi
 export PATH="~/.bin:$PATH"
