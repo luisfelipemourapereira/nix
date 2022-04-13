c8b985fd1408f379cc0da5c7c8a1dc1417da82d6
luis de siqueira <luis@pleme.io>
2022-04-13 16:09:13 -0700

    disable tmux

--- users/luis/zprofile.sh
+++ users/luis/zprofile.sh
@@ -3,8 +3,10 @@ if [ -n "${commands[fzf - share]}" ]; then
 	source "$(fzf-share)/key-bindings.zsh"
 	source "$(fzf-share)/completion.zsh"
 fi
-# if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
-# 	[ -z "$TMUX_PROJECT" ] && export TMUX_PROJECT=main
-# 	tmux attach -t $TMUX_PROJECT || tmux new -s $TMUX_PROJECT
-# fi
+! [ -e $HOME/.tmux.conf ] &&
+	ln -s $HOME/.config/tmux/tmux.conf $HOME/.tmux.conf
+if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
+	[ -z "$TMUX_PROJECT" ] && export TMUX_PROJECT=main
+	tmux attach -t $TMUX_PROJECT || tmux new -s $TMUX_PROJECT
+fi
 export PATH="~/.bin:$PATH"
