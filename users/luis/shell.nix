e6da4a8514c3ef6a2420a3e200ad10c9c5d2f8e6
luis de siqueira <luis@pleme.io>
2022-04-12 17:55:27 -0700

    functional tfswitch

--- users/luis/shell.nix
+++ users/luis/shell.nix
@@ -11,7 +11,7 @@
     pbcopy = "xsel --clipboard --input";
     pbpaste = "xsel --clipboard --output";
     vim = "nvim -u ~/.config/nvim/init.lua";
-    tfswitch = "sudo tfswitch -b /run/current-system/sw/bin/terraform";
+    # tfswitch = "tfswitch -b /run/current-system/sw/bin/terraform";
   };
 
   programs.zsh.initExtra = builtins.readFile ./zprofile.sh;
--- users/luis/zprofile.sh
+++ users/luis/zprofile.sh
@@ -3,3 +3,4 @@ if [ -n "${commands[fzf-share]}" ]; then
   source "$(fzf-share)/key-bindings.zsh"
   source "$(fzf-share)/completion.zsh"
 fi
+export PATH="~/.bin:$PATH"
