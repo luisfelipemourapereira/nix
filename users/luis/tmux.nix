562117eb4c42772ef053ba38dd89448c2fe7bd25
luis de siqueira <luis@pleme.io>
2022-04-13 14:06:46 -0700

    add tmux

--- users/luis/tmux.nix
+++ users/luis/tmux.nix
@@ -1,4 +1,4 @@
 { pkgs, lib, config, specialArgs, options, modulesPath, nixosConfig, osConfig }: {
   programs.tmux.enable = true;
-  programs.tmux.extraConfig = ./tmux.conf;
+  programs.tmux.extraConfig = builtins.readFile ./tmux.conf;
 }
