eab01bd63a793ce055cd080f4c182c279c817b91
luis de siqueira <luis@pleme.io>
2022-04-16 12:03:56 -0700

    turn on fzf history

--- users/luis/zprofile.sh
+++ users/luis/zprofile.sh
@@ -1,5 +1,5 @@
 bindkey '^R' history-incremental-search-backward
-if [ -n "${commands[fzf - share]}" ]; then
+if [ -n "${commands[fzf-share]}" ]; then
 	source "$(fzf-share)/key-bindings.zsh"
 	source "$(fzf-share)/completion.zsh"
 fi
