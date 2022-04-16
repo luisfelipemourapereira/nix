481d86815b66b3445fb9b1584de5061187c71de8
luis de siqueira <luis@pleme.io>
2022-04-16 16:45:57 -0700

    autorandr module

--- nodes/rio/derivations/pkgs/autorandr-rs/default.nix
+++ nodes/rio/derivations/pkgs/autorandr-rs/default.nix
@@ -6,9 +6,9 @@ rustPlatform.buildRustPackage rec {
     owner = "theotherjimmy";
     repo = name;
     rev = "51a705e3a3761d32b9e068f48fc34722ca3d2ab7";
-    hash = "sha256-ryK7/VYxo/ssMBYmGCJBk7sz+pp2MNPhHJx9LNcm6Lc=";
+    hash = "sha256-ryK7/VYxo/ssMBYmGCJBk7sz+pp2MNPhHJx9LNcm6Lc";
   };
-  cargoHash = "sha256-Rb81EXPqb7ydz5FA8AHwjnNIxp0Mr8vVRPx0sGIXqyY=";
+  cargoHash = "sha256-bJXP4L2N3Jg5+hBP5NMs6JT3n2wUqw4y07ycHLNSDZ4=";
   nativeBuildInputs = [ scdoc installShellFiles ];
   preFixup = ''
     installManPage $releaseDir/build/${name}-*/out/${name}.1
