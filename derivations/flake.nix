c541de6a16c2794c0c1a49bf07bcbe87a98681fb
luis de siqueira <luis@pleme.io>
2022-04-16 16:19:41 -0700

    nvidia configuration

--- derivations/flake.nix
+++ derivations/flake.nix
@@ -12,20 +12,6 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      # Generate a user-friendly version number
-      # version = builtins.substring 0 8 self.lastModifiedDate;
-
-      # systems to support
-      # supportedSystems = [ "x86_64-linux" ];
-      packages.x86_64-linux.autorandr-rs = pkgs.callPackage ./autorandr-rs/default.nix { };
-
-      # exposed packages
-      # packages = {
-      #   x86_64-linux =
-      #     with import nixpkgs { system = "x86_64-linux"; };
-      #     stdenv.mkDerivation {
-      #       name = "hello";
-      #     };
-      # };
+      packages.x86_64-linux.autorandr-rs = pkgs.callPackage ./pkgs/autorandr-rs/default.nix { };
     };
 }
