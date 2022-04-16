44b06cf00d5485d723ef3f3548f0bf3a4db325ac
luis de siqueira <luis@pleme.io>
2022-04-15 23:52:42 -0700

    learnings about modules and derivations

---
+++ derivations/flake.nix
@@ -0,0 +1,11 @@
+###############################################################################
+# multiple system identities here
+#
+# .#rio = laptop
+###############################################################################
+
+{
+  description = "local packages";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  outputs = {}: { };
+}
