44538407b2674208e24a6724f076118ea4532a9a
luis de siqueira <luis@pleme.io>
2022-04-16 17:06:06 -0700

    remove message

--- users/luis/zprofile.sh
+++ users/luis/zprofile.sh
@@ -1,12 +1,10 @@
 bindkey '^R' history-incremental-search-backward
-# if [ -n "${commands["fzf-share"]}" ]; then
-# 	source "$(fzf-share)/key-bindings.zsh"
-# 	source "$(fzf-share)/completion.zsh"
-# fi
-# ! [ -e $HOME/.tmux.conf ] &&
-# 	ln -s $HOME/.config/tmux/tmux.conf $HOME/.tmux.conf
-# if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
-# 	[ -z "$TMUX_PROJECT" ] && export TMUX_PROJECT=main
-# 	tmux attach -t $TMUX_PROJECT || tmux new -s $TMUX_PROJECT
-# fi
+source "$(fzf-share)/key-bindings.zsh"
+source "$(fzf-share)/completion.zsh"
+! [ -e $HOME/.tmux.conf ] &&
+	ln -s $HOME/.config/tmux/tmux.conf $HOME/.tmux.conf
+if command -v tmux &>/dev/null && [ -z "$TMUX" ]; then
+	[ -z "$TMUX_PROJECT" ] && export TMUX_PROJECT=main
+	tmux attach -t $TMUX_PROJECT || tmux new -s $TMUX_PROJECT
+fi
 export PATH="~/.bin:$PATH"
