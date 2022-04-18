b52784354d2775f815ace9ca67ec115d2120897d
luis de siqueira <luis@pleme.io>
2022-04-17 23:11:58 -0700

    make internal requires relative

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 {
   description = "Use like: nixos-rebuild switch --flake '.#rio'";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=6140c314d5850406dddc78e55dcc8716ea111ee4";
-  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.url = "github:nix-community/home-manager?ref=742c6cb3e9d866e095c629162fe5faf519adeb26";
   inputs.cauda.url = "git+ssh://git@github.com/luisfelipemourapereira/cauda?ref=main";
   outputs = { nixpkgs, home-manager, self, cauda }: {
     nixosConfigurations = {
