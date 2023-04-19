73a9afc251a36c3897534e27c0cfa269a5580a58
Luis De Siqueira <ldesiqueira@pinger.com>
2023-04-19 14:29:10 -0400

    dockerfile-language-server -> dockerls

--- modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/dockerls.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/dockerls.lua
@@ -3,16 +3,14 @@ local M = {}
 function M.setup(opts)
 	local lsputils = require("lsp.utils")
 
-	-- override dockerfile-language-server
-	local dockerfile_language_server_opts = lsputils.merge(
+	-- override dockerls
+	local dockerls_opts = lsputils.merge(
 		{},
 		opts
 	)
 
-	-- enable dockerfile-language-server
-	require("lspconfig").dockerfile_language_server.setup(
-		dockerfile_language_server_opts
-	)
+	-- enable dockerls
+	require("lspconfig").dockerls.setup(dockerls_opts)
 end
 
 return M
