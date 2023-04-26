86344f5a764f371f18f8ba312d3b1ffff3c9c6d8
luis <ldesiqueira@pinger.com>
2023-04-25 23:31:23 -0400

    move format to which-key configuration

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/folke/which-key.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/folke/which-key.lua
@@ -19,11 +19,10 @@ function M.setup()
 		nowait = false,
 	}
 
-	local mappings = {
-		["ff"] = { "<cmd>lua vim.lsp.buff.format()<CR>", "Format" },
-		["t"]  = { "<cmd>Telescope find_files<CR>", "Find" },
-		["w"]  = { "<cmd>update!<CR>", "Save" },
-		["q"]  = { "<cmd>q!<CR>", "Quit" },
+	local leader_mappings = {
+		["t"] = { "<cmd>Telescope find_files<CR>", "Find" },
+		["w"] = { "<cmd>update!<CR>", "Save" },
+		["q"] = { "<cmd>q!<CR>", "Quit" },
 
 		b = {
 			name = "Buffer",
@@ -31,8 +30,19 @@ function M.setup()
 		},
 	}
 
+	local non_leader_opts = {
+		mode = "n",
+		silent = true,
+		noremap = true
+	}
+
+	local non_leader_mappings = {
+		["ff"] = { "<cmd>lua vim.lsp.buf.format()<CR>", "Format" },
+	}
+
 	wk.setup(conf)
-	wk.register(mappings, opts)
+	wk.register(leader_mappings, opts)
+	wk.register(non_leader_mappings, non_leader_opts)
 end
 
 return M
