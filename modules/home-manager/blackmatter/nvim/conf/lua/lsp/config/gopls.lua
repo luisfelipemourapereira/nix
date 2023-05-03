54dc1b790320df0d7a5ecf7d9acce0b4317c45ee
luis <ldesiqueira@pinger.com>
2023-05-02 18:29:40 -0700

    gopls

--- modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/gopls.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/gopls.lua
@@ -5,12 +5,18 @@ function M.setup(opts)
 
 	-- override gopls
 	local gopls_opts = lsputils.merge(
-		{},
+		{
+			settings = {
+				gopls = {
+					gofumpt = true
+				}
+			}
+		},
 		opts
 	)
 
 	-- enable gopls
-	require("lspconfig").bashls.setup(gopls_opts)
+	require("lspconfig").gopls.setup(gopls_opts)
 end
 
 return M
