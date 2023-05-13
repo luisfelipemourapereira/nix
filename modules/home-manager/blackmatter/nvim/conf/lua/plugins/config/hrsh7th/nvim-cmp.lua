1c08938ee3f2f48f43145f27c86f89260ad33f32
t3rro <t3rro@protonmail.com>
2023-05-13 16:18:18 -0700

    add plugin: wk/register

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/hrsh7th/nvim-cmp.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/hrsh7th/nvim-cmp.lua
@@ -1,6 +1,10 @@
 local M = {}
 
+--[[
 -- Check if there are non-whitespace characters before the cursor
+-- @return boolean: true if non-whitespace characters exist before the cursor, false otherwise
+local function has_words_before()
+--]]
 local function has_words_before()
 	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 		return false
@@ -9,45 +13,25 @@ local function has_words_before()
 	return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
--- Check if 'kyazdani42/nvim-web-devicons' plugin is installed
+--[[
+----- Check if 'kyazdani42/nvim-web-devicons' plugin is installed
+-- @return boolean: true if the plugin is installed, false otherwise 
+--]]
 local function is_codicons_installed()
 	local ok, _ = pcall(require, 'nvim-web-devicons')
 	return ok
 end
 
--- Set up lspkind configuration based on the provided kind_config ("text" or "codicons")
+--[[
+----- Set up lspkind configuration based on the provided kind_config ("text" or "codicons")
+-- @param kind_config string: The kind of configuration to set up (either "text" or "codicons")
+--]]
 local function set_lspkind_config(kind_config)
 	local symbol_map = {}
 	local lspkind = require('lspkind')
 
 	if kind_config == "text" then
-		symbol_map = {
-			Text = "[-]",
-			Method = "[M]",
-			Function = "[ƒ]",
-			Constructor = "[C]",
-			Field = "[.]",
-			Variable = "[V]",
-			Class = "[#]",
-			Interface = "[I]",
-			Module = "[M]",
-			Property = "[P]",
-			Unit = "[U]",
-			Value = "[v]",
-			Enum = "[E]",
-			Keyword = "[K]",
-			Snippet = "[S]",
-			Color = "[C]",
-			File = "[F]",
-			Reference = "[R]",
-			Folder = "[D]",
-			EnumMember = "[e]",
-			Constant = "[C]",
-			Struct = "[S]",
-			Event = "[@]",
-			Operator = "[O]",
-			TypeParameter = "[T]"
-		}
+		symbol_map = M.getTextSymbolMap()
 	elseif kind_config == "codicons" and is_codicons_installed then
 		symbol_map = require('lspkind').presets.default
 	else
@@ -56,97 +40,122 @@ local function set_lspkind_config(kind_config)
 
 	lspkind.init({ symbol_map = symbol_map })
 
+	M.setup_cmp()
+end
+
+--[[
+-- get the local text symbol map
+--]]
+function M.getTextSymbolMap()
+	return {
+		Text = "[-]",
+		Method = "[M]",
+		Function = "[ƒ]",
+		Constructor = "[C]",
+		Field = "[.]",
+		Variable = "[V]",
+		Class = "[#]",
+		Interface = "[I]",
+		Module = "[M]",
+		Property = "[P]",
+		Unit = "[U]",
+		Value = "[v]",
+		Enum = "[E]",
+		Keyword = "[K]",
+		Snippet = "[S]",
+		Color = "[C]",
+		File = "[F]",
+		Reference = "[R]",
+		Folder = "[D]",
+		EnumMember = "[e]",
+		Constant = "[C]",
+		Struct = "[S]",
+		Event = "[@]",
+		Operator = "[O]",
+		TypeParameter = "[T]"
+	}
+end
+
+--[[
+-- setup completion
+--]]
+function M.setup_cmp()
 	local cmp = require("cmp")
+	local lspkind = require('lspkind')
+
 	cmp.setup(
 		{
 			formatting = {
 				format = lspkind.cmp_format({
-					-- mode = 'symbol',
-					-- maxwidth = 50,
-					-- ellipsis_char = '...',
 					before = function(entry, vim_item)
-						-- TODO: work in progress to give better completion information
-						-- vim_item.kind = symbol_map[vim_item.kind]
-						-- vim_item.menu = string.format("%s %s", vim_item.menu, vim_item.kind)
-
-						-- show source of completion items
-						-- showing source is useful for many things but especially
-						-- testing out functionality of new language servers
 						local source = entry.source.name
-
-						-- EXAMPLE: in case we only want to show source
-						-- on certain sources this incantation should work
-						-- if source == 'nvim_lsp' then
-						-- 	source = entry.source.client.name
-						-- end
-
 						vim_item.menu = string.format(
 							"%s (%s)",
 							vim_item.menu or "",
 							source
 						)
-
 						return vim_item
 					end
 				}),
 			},
-			-- TODO: consider adding when doing snips
 			snippet = {
 				expand = function(args)
 					require('luasnip').lsp_expand(args.body)
 				end,
 			},
-			mapping = {
-				["<Tab>"] = vim.schedule_wrap(function(fallback)
-					if cmp.visible() and has_words_before() then
-						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-					else
-						fallback()
-					end
-				end),
-				['<C-y>'] = cmp.config.disable,
-				['<CR>'] = cmp.mapping.confirm({ select = true }),
-				['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-				['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-				['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-				['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-				['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-				['<C-e>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close(), }),
-			},
-			sources = cmp.config.sources(
-				{
-					{ name = 'nvim_lsp' },
-					{ name = 'copilot' },
-					-- TODO: currently has bugs
-					-- { name = 'codeium' },
-					{ name = 'treesitter' },
-					-- TODO: consider adding when doing snips
-					{ name = 'luasnip' },
-					{ name = 'buffer' },
-					{ name = 'path' },
-					-- TODO: has a bug with *, add later
-					{ name = 'cmdline' },
-				}
-			)
+			mapping = M.getCmpMapping(),
+			sources = cmp.config.sources(M.getCmpSources())
 		}
 	)
 end
 
+--[[
+-- completion key mappings
+--]]
+function M.getCmpMapping()
+	local cmp = require("cmp")
+	return {
+		["<Tab>"] = vim.schedule_wrap(function(fallback)
+			if cmp.visible() and has_words_before() then
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			else
+				fallback()
+			end
+		end),
+		['<C-y>'] = cmp.config.disable,
+		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+		['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+		['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+		['<C-e>'] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close(), }),
+	}
+end
+
+--[[
+-- completion sources
+--]]
+function M.getCmpSources()
+	return {
+		{ name = 'nvim_lsp' },
+		{ name = 'copilot' },
+		{ name = 'treesitter' },
+		{ name = 'luasnip' },
+		{ name = 'buffer' },
+		{ name = 'path' },
+		{ name = 'cmdline' },
+	}
+end
+
+--[[
+-- put it all together
+--]]
 function M.setup()
 	vim.cmd [[ set completeopt=menu,menuone,noselect ]]
 	local map = require("utils.map")
-
-	-- TODO: devicons seem to be detected but still show weird little boxes
-	-- local default_kind_config = is_codicons_installed() and "codicons" or "text"
-	-- set_lspkind_config(default_kind_config)
-	--
-	-- setting text directly since codicons configuration is still broken
 	set_lspkind_config("text")
-
-	-- with only the mappings above when you press c-n on insert with nothing inserted
-	-- it brings up the ins-completion native from neovim while you want to do cmp.complete()
-	-- instead as if you did C-Space
-	map('i', '<c-n>', "<cmd>lua require('cmp').complete()<CR>", {
+	map('i', '<C-n>', "<cmd>lua require('cmp').complete()<CR>", {
 		noremap = false, expr = false
 	})
 end
