25af234d828d9134019fa090be76c7f1ef411f18
t3rro <t3rro@protonmail.com>
2023-05-13 16:34:35 -0700

    lazy load instead

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/L3MON4D3/LuaSnip.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/L3MON4D3/LuaSnip.lua
@@ -4,6 +4,7 @@ function M.setup()
 		history = false,
 		updateevents = 'TextChanged,TextChangedI',
 	}
+	-- do not use .load() it will be incredibly slow
 	require("luasnip/loaders/from_vscode").lazy_load()
 end
 
