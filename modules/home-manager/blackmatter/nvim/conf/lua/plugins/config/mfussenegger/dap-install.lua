87c1b15a0b99e2567108f40801e31f1b5f1b313d
t3rro <t3rro@protonmail.com>
2023-05-13 10:46:08 -0700

    add dap-install

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/mfussenegger/dap-install.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/mfussenegger/dap-install.lua
@@ -15,52 +15,52 @@ local function configure()
 		installation_path = vim.fn.stdpath "data" .. "/dapinstall/",
 	}
 
-	-- local dap_breakpoint = {
-	-- 	error = {
-	-- 		text = "🟥",
-	-- 		texthl = "LspDiagnosticsSignError",
-	-- 		linehl = "",
-	-- 		numhl = "",
-	-- 	},
-	-- 	rejected = {
-	-- 		text = "",
-	-- 		texthl = "LspDiagnosticsSignHint",
-	-- 		linehl = "",
-	-- 		numhl = "",
-	-- 	},
-	-- 	stopped = {
-	-- 		text = "⭐️",
-	-- 		texthl = "LspDiagnosticsSignInformation",
-	-- 		linehl = "DiagnosticUnderlineInfo",
-	-- 		numhl = "LspDiagnosticsSignInformation",
-	-- 	},
-	-- }
+	local dap_breakpoint = {
+		error = {
+			text = "🟥",
+			texthl = "LspDiagnosticsSignError",
+			linehl = "",
+			numhl = "",
+		},
+		rejected = {
+			text = "",
+			texthl = "LspDiagnosticsSignHint",
+			linehl = "",
+			numhl = "",
+		},
+		stopped = {
+			text = "⭐️",
+			texthl = "LspDiagnosticsSignInformation",
+			linehl = "DiagnosticUnderlineInfo",
+			numhl = "LspDiagnosticsSignInformation",
+		},
+	}
 
-	-- vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
-	-- vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
-	-- vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
+	vim.fn.sign_define("DapBreakpoint", dap_breakpoint.error)
+	vim.fn.sign_define("DapStopped", dap_breakpoint.stopped)
+	vim.fn.sign_define("DapBreakpointRejected", dap_breakpoint.rejected)
 end
 
--- local function configure_exts()
--- 	require("nvim-dap-virtual-text").setup {
--- 		commented = true,
--- 	}
---
--- 	local dap, dapui = require "dap", require "dapui"
--- 	dapui.setup {}
---
--- 	-- close the dapui when the debugger exits
--- 	dap.listeners.after.event_initialized["dapui_config"] = function()
--- 		dapui.close()
--- 	end
--- 	dap.listeners.before.event_terminated["dapui_config"] = function()
--- 		dapui.close()
--- 	end
--- 	dap.listeners.before.event_exited["dapui_config"] = function()
--- 		dapui.close()
--- 	end
---
--- end
+local function configure_exts()
+	require("nvim-dap-virtual-text").setup {
+		commented = true,
+	}
+
+	local dap, dapui = require "dap", require "dapui"
+	dapui.setup {}
+
+	-- close the dapui when the debugger exits
+	dap.listeners.after.event_initialized["dapui_config"] = function()
+		dapui.close()
+	end
+	dap.listeners.before.event_terminated["dapui_config"] = function()
+		dapui.close()
+	end
+	dap.listeners.before.event_exited["dapui_config"] = function()
+		dapui.close()
+	end
+
+end
 
 -- local function configure_debuggers()
 -- 	local languages = { "lua", "python", "rust", "go" }
@@ -71,7 +71,7 @@ end
 
 function M.setup()
 	configure()
-	-- configure_exts()
+	configure_exts()
 	-- configure_debuggers()
 end
 
