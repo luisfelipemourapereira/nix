754d8a02db1ed5aed83eaaacde9e7edd82dcc6c1
t3rro <t3rro@protonmail.com>
2023-05-13 13:40:11 -0700

    manage plugin: neotest-jest

---
+++ modules/home-manager/blackmatter/nvim/plugins/nvim-neotest/neotest-go/default.nix
@@ -0,0 +1,22 @@
+{ lib, config, ... }:
+with lib;
+let
+  author = "nvim-neotest";
+  name = "neotest-go";
+  url = "https://github.com/${author}/${name}";
+  ref = "master";
+  rev = "4e7a87967e45f2b357fd546b0b7877774191772e";
+  plugPath = ".local/share/nvim/site/pack/${author}/start/${name}";
+  cfg = config.blackmatter.programs.nvim.plugins.${author}.${name};
+in
+{
+  options.blackmatter.programs.nvim.plugins.${author}.${name}.enable =
+    mkEnableOption "${author}/${name}";
+
+  config = mkMerge [
+    (mkIf cfg.enable {
+      home.file."${plugPath}".source =
+        builtins.fetchGit { inherit ref rev url; };
+    })
+  ];
+}
