f75d943f0bcefa15272ee58092940ebd3bc15c9a
t3rro <t3rro@protonmail.com>
2023-05-13 14:30:23 -0700

    add plugin: nvim-treesitter/nvim-treesitter-textobjects

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/nvim-treesitter/nvim-treesitter.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/nvim-treesitter/nvim-treesitter.lua
@@ -31,6 +31,55 @@ function M.setup()
 		};
 		-- enable vim-matchup integration
 		matchup = { enable = true, },
+		-- nvim-treesitter-textobjects
+		textobjects = {
+			lsp_interop = {
+				enable = true,
+				peek_definition_code = {
+					["df"] = "@function.outer",
+					["dF"] = "@class.outer",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>rx"] = "@parameter.inner",
+				},
+				swap_previous = {
+					["<leader>rX"] = "@parameter.inner",
+				},
+			},
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+				}
+			}
+		},
 	}
 	-- TODO: folding is a bit buggy troubleshoot later
 	-- vim.cmd [[ set foldmethod=expr ]]
