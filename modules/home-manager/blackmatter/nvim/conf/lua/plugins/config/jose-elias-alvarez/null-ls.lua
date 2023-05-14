abbcd1576a6cbdf89455b627da5fb1485afd57a0
t3rro <t3rro@protonmail.com>
2023-05-13 17:01:44 -0700

    hookup to null-ls

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/jose-elias-alvarez/null-ls.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/jose-elias-alvarez/null-ls.lua
@@ -5,13 +5,19 @@ function M.setup()
 		nls.builtins.formatting.black,
 		nls.builtins.formatting.shfmt,
 		nls.builtins.formatting.prettier,
-		nls.builtins.formatting.terraform_fmt.with({
-			filetypes = { 'terraform', 'tf', 'hcl' },
-		}),
 		nls.builtins.code_actions.gitsigns,
 		nls.builtins.code_actions.eslint_d,
 		nls.builtins.code_actions.gitrebase,
 		nls.builtins.code_actions.refactoring,
+		nls.builtins.formatting.terraform_fmt.with(
+			{
+				filetypes = {
+					'terraform',
+					'tf',
+					'hcl'
+				},
+			}
+		),
 	}
 	nls.setup({ sources = nlssources })
 end
