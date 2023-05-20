2b25a008a036e4a812561767114ac6da119d3112
t3rro <t3rro@protonmail.com>
2023-05-20 09:30:31 -0700

    pipewireconfiguration

--- modules/nixos/blackmatter/pipewire/default.nix
+++ modules/nixos/blackmatter/pipewire/default.nix
@@ -20,38 +20,40 @@ in
           pulse.enable = true;
           jack.enable = true;
 
-          media-session.config.bluez-monitor.rules = [
-            {
-              # Matches all cards
-              matches = [{ "device.name" = "~bluez_card.*"; }];
-              actions = {
-                "update-props" = {
-                  "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-                  # mSBC is not expected to work on all headset + adapter combinations.
-                  "bluez5.msbc-support" = true;
-                  # SBC-XQ is not expected to work on all headset + adapter combinations.
-                  "bluez5.sbc-xq-support" = true;
-                };
-              };
-            }
-            {
-              matches = [
-                # Matches all sources
-                { "node.name" = "~bluez_input.*"; }
-                # Matches all outputs
-                { "node.name" = "~bluez_output.*"; }
-              ];
-            }
-          ];
+          # TODO: remove this nix migration is complete
+          # media-session.config.bluez-monitor.rules = [
+          #   {
+          #     # Matches all cards
+          #     matches = [{ "device.name" = "~bluez_card.*"; }];
+          #     actions = {
+          #       "update-props" = {
+          #         "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+          #         # mSBC is not expected to work on all headset + adapter combinations.
+          #         "bluez5.msbc-support" = true;
+          #         # SBC-XQ is not expected to work on all headset + adapter combinations.
+          #         "bluez5.sbc-xq-support" = true;
+          #       };
+          #     };
+          #   }
+          #   {
+          #     matches = [
+          #       # Matches all sources
+          #       { "node.name" = "~bluez_input.*"; }
+          #       # Matches all outputs
+          #       { "node.name" = "~bluez_output.*"; }
+          #     ];
+          #   }
+          # ];
 
-          config.pipewire = {
-            "link.max-buffers" = 64;
-            "log.level" = 2;
-            "default.clock.rate" = 48000;
-            "default.clock.quantum" = 1024;
-            "default.clock.min-quantum" = 32;
-            "default.clock.max-quantum" = 8192;
-          };
+          # TODO: remove this nix migration is complete
+          # config.pipewire = {
+          #   "link.max-buffers" = 64;
+          #   "log.level" = 2;
+          #   "default.clock.rate" = 48000;
+          #   "default.clock.quantum" = 1024;
+          #   "default.clock.min-quantum" = 32;
+          #   "default.clock.max-quantum" = 8192;
+          # };
         };
       })
     ];
