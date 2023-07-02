74891627f4d619f1d0bca1515bfe5e5d80aec543
luis <ldesiqueira@pinger.com>
2023-07-01 22:17:36 -0300

    add a basic direnvrc.sh

--- modules/home-manager/blackmatter/shell/zsh/default.nix
+++ modules/home-manager/blackmatter/shell/zsh/default.nix
@@ -23,6 +23,7 @@ in
       	home.packages = [ cfg.shell.zsh.package ];
   	xdg.configFile."shellz/rbenv/main.sh".source = ./rbenv/main.sh;
   	xdg.configFile."shellz/direnv/main.sh".source = ./direnv/main.sh;
+  	home.file.".direnvrc".source = ./direnv/direnvrc.sh;
   	xdg.configFile."shellz/path/main.sh".source = ./path/main.sh;
   	xdg.configFile."shellz/history/main.sh".source = ./history/main.sh;
   	xdg.configFile."shellz/sheldon/main.sh".source = ./sheldon/main.sh;
