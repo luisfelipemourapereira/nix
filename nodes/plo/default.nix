cdd051909959ddd70f78e110ff4be9e7e379b0af
luis <luisfelipemourapereira@protonmail.com>
2023-07-30 18:38:06 -0700

    init

--- nodes/plo/default.nix
+++ nodes/plo/default.nix
@@ -14,15 +14,23 @@
   blackmatter.users.enable = true;
   blackmatter.fonts.enable = true;
   blackmatter.global.enable = true;
-  blackmatter.xserver.enable = true;
-  blackmatter.display.enable = true;
-  blackmatter.pipewire.enable = true;
   blackmatter.services.enable = true;
   blackmatter.packages.enable = true;
   blackmatter.programs.enable = true;
-  blackmatter.networking.enable = true;
   blackmatter.hardware.plo.enable = true;
+
+  # audio
+  blackmatter.pipewire.enable = true;
+
+  # network
+  blackmatter.networking.enable = true;
+
+  # virtualization
   blackmatter.virtualization.enable = true;
+
+  # GUI
+  blackmatter.xserver.enable = false;
+  blackmatter.display.enable = false;
 
   antimatter.services.consul.enable = true;
   antimatter.services.nomad.enable = true;
