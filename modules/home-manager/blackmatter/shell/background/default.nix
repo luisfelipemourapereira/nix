9b1cbf34ecab2f93122f541449ec0af3bc510f03
luis <ldesiqueira@pinger.com>
2023-08-01 11:03:19 -0300

    MBG

--- modules/home-manager/blackmatter/shell/background/default.nix
+++ modules/home-manager/blackmatter/shell/background/default.nix
@@ -13,12 +13,12 @@ in
   config = mkMerge [
     (mkIf cfg.shell.background.enable {
       home.file."backgrounds/nord/tools".source = builtins.fetchGit {
-        url = "ssh://git@github.com/arcticicestudio/nord";
+        url = "https://github.com/arcticicestudio/nord.git";
         ref = "develop";
         rev = "c93f12b23baac69a92e7559f69e7a60c20b9da0d";
       };
       home.file."backgrounds/nord/backgrounds".source = builtins.fetchGit {
-        url = "git@github.com:dxnst/nord-backgrounds.git";
+        url = "https://github.com/dxnst/nord-backgrounds.git";
         ref = "main";
         rev = "c47d6b8b0ea391fabbb79aa005703ae5549ffdc4";
       };
