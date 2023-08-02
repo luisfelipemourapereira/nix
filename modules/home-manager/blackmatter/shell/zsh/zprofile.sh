5a0487bb1c376f86c2344658d35901e872e63ca7
luis <ldesiqueira@pinger.com>
2023-08-02 11:29:28 -0300

    tupa

--- modules/home-manager/blackmatter/shell/zsh/zprofile.sh
+++ modules/home-manager/blackmatter/shell/zsh/zprofile.sh
@@ -11,29 +11,29 @@ export PATH=/etc/profiles/per-user/$USER/bin:$PATH
 export PATH=~/bin:$PATH
 
 function load_shellz_mod() {
-	[ -d ~/.config/shellz/$1 ] && source ~/.config/shellz/$1/main.sh
+  [ -d ~/.config/shellz/$1 ] && source ~/.config/shellz/$1/main.sh
 }
 
 # disabled sections of zsh configuration
 # rbenv
 declare -a shellz_mods=(
-	direnv
-	path
-	xdg
-	nix
-	tmux
-	zstyle
-	completions
-	ssh_agent
+  direnv
+  path
+  xdg
+  nix
+  tmux
+  zstyle
+  completions
+  ssh_agent
 )
 
 for m in "${shellz_mods[@]}"; do
-	load_shellz_mod $m
+  load_shellz_mod $m
 done
 
 # load gh token from sops-nix location if exists
 [ -e ~/.config/gh/env/GH_TOKEN ] &&
-	export GH_TOKEN=$(cat ~/.config/gh/env/GH_TOKEN)
+  export GH_TOKEN=$(cat ~/.config/gh/env/GH_TOKEN)
 
 # some aliases
 alias grep=rg
@@ -53,3 +53,9 @@ eval "$(zoxide init zsh)"
 [ -f ~/.config/shellz/hooks/main.sh ] && source ~/.config/shellz/hooks/main.sh
 
 # end zsh config
+#
+# export PERL_LOCAL_LIB_ROOT="$PERL_LOCAL_LIB_ROOT:/root/perl5";
+export PERL_MB_OPT="--install_base /root/perl5"
+export PERL_MM_OPT="INSTALL_BASE=/root/perl5"
+export PERL5LIB="/root/perl5/lib/perl5:$PERL5LIB"
+export PATH="/root/perl5/bin:$PATH"
