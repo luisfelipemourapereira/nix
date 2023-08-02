443be493f6ade665b1f309ee0edb70282615306b
luis <ldesiqueira@pinger.com>
2023-08-01 22:49:04 -0300

    tupa

--- users/root/tupa/home.nix
+++ users/root/tupa/home.nix
@@ -1,9 +1,23 @@
 { outputs, ... }:
+let
+  home = "/root";
+  user = "root";
+in
 {
   imports = [
     outputs.homeManagerModules.blackmatter
-    ../common
   ];
+
+  home.username = user;
+  home.homeDirectory = home;
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    AWS_REGION = "us-west-2";
+    AWS_DEFAULT_REGION = "us-west-2";
+    AWS_PROFILE = "pinger-organization";
+  };
+
   blackmatter.programs.nvim.enable = true;
   blackmatter.shell.enable = true;
 }
