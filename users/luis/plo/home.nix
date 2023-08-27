2fb78e33b229d944f8ceb600c44e624fdc4b4ba5
luis <luisfelipemourapereira@protonmail.com>
2023-08-27 00:25:45 -0400

    turn up alacritty

--- users/luis/plo/home.nix
+++ users/luis/plo/home.nix
@@ -8,7 +8,7 @@
   blackmatter.programs.nvim.enable = true;
   blackmatter.shell.enable = true;
   blackmatter.desktop.enable = false;
-  blackmatter.desktop.alacritty.enable = false;
+  blackmatter.desktop.alacritty.enable = true;
   blackmatter.desktop.monitors = {
     main = {
       name = "DP-2";
