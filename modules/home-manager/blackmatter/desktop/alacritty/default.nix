33e03550f5dfaf12251495603eff10598bd8b810
luis <luisfelipemourapereira@protonmail.com>
2023-08-27 00:54:49 -0400

    use local alacritty not nix alacritty

--- modules/home-manager/blackmatter/desktop/alacritty/default.nix
+++ modules/home-manager/blackmatter/desktop/alacritty/default.nix
@@ -7,11 +7,14 @@ in
   options = {
     blackmatter = {
       desktop.alacritty.enable = mkEnableOption "desktop.alacritty";
+      desktop.alacritty.config.enable = mkEnableOption "desktop.alacritty.config";
     };
   };
   config = mkMerge [
     (mkIf cfg.desktop.alacritty.enable {
       programs.alacritty.enable = true;
+    })
+    (mkIf cfg.desktop.alacritty.config.enable {
       xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
     })
   ];
--- users/luis/plo/home.nix
+++ users/luis/plo/home.nix
@@ -8,7 +8,8 @@
   blackmatter.programs.nvim.enable = true;
   blackmatter.shell.enable = true;
   blackmatter.desktop.enable = false;
-  blackmatter.desktop.alacritty.enable = true;
+  blackmatter.desktop.alacritty.enable = false;
+  blackmatter.desktop.alacritty.config.enable = true;
   blackmatter.desktop.monitors = {
     main = {
       name = "DP-2";
