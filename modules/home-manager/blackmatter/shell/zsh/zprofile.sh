c97de7aa646cf2271642c2f10f61755c26d60ace
luis <luisfelipemourapereira@protonmail.com>
2023-12-03 11:19:38 +0100

    android shell setup

--- modules/home-manager/blackmatter/shell/zsh/zprofile.sh
+++ modules/home-manager/blackmatter/shell/zsh/zprofile.sh
@@ -46,6 +46,7 @@ eval "$(zoxide init zsh)"
 ###############################################################################
 
 export ANDROID_HOME=/usr/lib/android-sdk
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 ###############################################################################
 # shellhooks
