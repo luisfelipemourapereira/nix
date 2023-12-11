85eb9e5cfe871593ba68d30b0073765abbbd04d0
luis <luisfelipemourapereira@protonmail.com>
2023-12-11 16:43:04 +0100

    build new rust version

--- modules/home-manager/blackmatter/shell/packages/rustlang/default.nix
+++ modules/home-manager/blackmatter/shell/packages/rustlang/default.nix
@@ -1,4 +1,14 @@
-pkgs: with pkgs; [
+pkgs:
+with pkgs;
+let
+  OurRustc = pkgs.rustc.overrideAttrs
+    (oldAttrs: rec {
+      version = "1.71.0";
+      sha256 = "0000000000000000000000000000000000000000000000000000"; # Placeholder hash
+      url = "https://static.rust-lang.org/dist/rustc-${version}-src.tar.gz";
+    });
+in
+[
   cargo-edit
   rust-code-analysis
   rust-analyzer
@@ -9,5 +19,5 @@ pkgs: with pkgs; [
   rustscan
   rustfmt
   rustcat
-  rustc
+  OurRustc
 ]
