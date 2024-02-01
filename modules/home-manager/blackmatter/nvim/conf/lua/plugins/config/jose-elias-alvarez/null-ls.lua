62f2c7c9a277c92743c14fc9b054b948886a3f59
luis <ldesiqueira@pinger.com>
2024-02-01 14:25:42 -0300

    add psalm config

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/jose-elias-alvarez/null-ls.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/config/jose-elias-alvarez/null-ls.lua
@@ -1,30 +1,33 @@
 local M = {}
 
 function M.setup()
-	local nls = require("null-ls")
+  local nls = require("null-ls")
 
-	local nlssources = {
-		nls.builtins.formatting.black,
-		nls.builtins.formatting.prettier,
-		-- commenting out rubocop so solargraph can take over
-		-- nls.builtins.formatting.rubocop,
-		nls.builtins.code_actions.gitsigns,
-		nls.builtins.code_actions.eslint_d,
-		nls.builtins.code_actions.gitrebase,
-		nls.builtins.code_actions.refactoring,
-		nls.builtins.formatting.terraform_fmt.with({
-			filetypes = { "terraform", "tf", "hcl" },
-		}),
-		-- nls.builtins.diagnostics.shellcheck.with({
-		-- 	extra_args = { "--format=json" },
-		-- 	diagnostics_format = "#{message}",
-		-- }),
-		nls.builtins.formatting.shfmt.with({
-			extra_args = { "-i", "2", "-sr" },
-		}),
-	}
+  local nlssources = {
+    nls.builtins.formatting.phpcsfixer.with({
+      command = "php-cs-fixer",
+    }),
+    nls.builtins.formatting.black,
+    nls.builtins.formatting.prettier,
+    -- commenting out rubocop so solargraph can take over
+    -- nls.builtins.formatting.rubocop,
+    nls.builtins.code_actions.gitsigns,
+    nls.builtins.code_actions.eslint_d,
+    nls.builtins.code_actions.gitrebase,
+    nls.builtins.code_actions.refactoring,
+    nls.builtins.formatting.terraform_fmt.with({
+      filetypes = { "terraform", "tf", "hcl" },
+    }),
+    -- nls.builtins.diagnostics.shellcheck.with({
+    -- 	extra_args = { "--format=json" },
+    -- 	diagnostics_format = "#{message}",
+    -- }),
+    nls.builtins.formatting.shfmt.with({
+      extra_args = { "-i", "2", "-sr" },
+    }),
+  }
 
-	nls.setup({ sources = nlssources })
+  nls.setup({ sources = nlssources })
 end
 
 return M
