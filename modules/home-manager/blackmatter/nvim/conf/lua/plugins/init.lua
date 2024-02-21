375cc01269067fc5ee3d15b12969b02d8c23f948
luis <ldesiqueira@pinger.com>
2024-02-21 10:26:22 -0300

    init

--- modules/home-manager/blackmatter/nvim/conf/lua/plugins/init.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/plugins/init.lua
@@ -1,25 +1,25 @@
 local M = {}
 
 function M.setup()
-	local utils = require('utils')
-	-- find everything in this directory
-	-- and run its setup hook.
-	utils.load_files('~/.config/nvim/lua/plugins/config')
+  local utils = require('utils')
+  -- find everything in this directory
+  -- and run its setup hook.
+  utils.load_files('~/.config/nvim/lua/plugins/config')
 
-	-- TODO: make a part of utils.load_files
-	require("lsp").setup()
+  -- TODO: make a part of utils.load_files
+  require("lsp").setup()
 
-	-- by default make gitblame on line off
-	vim.cmd [[ let g:gitblame_enabled = 0 ]]
+  -- by default make gitblame on line off
+  vim.cmd [[ let g:gitblame_enabled = 0 ]]
 
-	-- recognize prisma files
-	vim.cmd([[ au BufNewFile,BufRead *.prisma set filetype=prisma ]])
+  -- recognize prisma files
+  vim.cmd([[ au BufNewFile,BufRead *.prisma set filetype=prisma ]])
 
-	-- load all language specific configs
-	utils.load_files('~/.config/nvim/lua/config/langs')
+  -- load all language specific configs
+  utils.load_files('~/.config/nvim/lua/config/langs')
 
-	-- load testing configurations
-	utils.load_files('~/.config/nvim/lua/config/testing')
+  -- load testing configurations
+  utils.load_files('~/.config/nvim/lua/config/testing')
 end
 
 return M
