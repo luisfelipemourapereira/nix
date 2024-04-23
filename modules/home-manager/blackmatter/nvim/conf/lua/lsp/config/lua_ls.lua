8758fec9ee5e37492a275138509558877963faf6
luis <ldesiqueira@pinger.com>
2024-04-23 19:32:25 -0300

    add ngx to lua globals

--- modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/lua_ls.lua
+++ modules/home-manager/blackmatter/nvim/conf/lua/lsp/config/lua_ls.lua
@@ -1,33 +1,38 @@
 local M = {}
 
 function M.setup(opts)
-	local lsputils = require("lsp.utils")
+  local lsputils = require("lsp.utils")
 
-	-- override lua_ls
-	local lua_ls_opts = lsputils.merge(
-		{
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-						path = vim.split(package.path, ";"),
-					},
-					workspace = {
-						maxPreload = 100000,
-						preloadFileSize = 1000,
-					},
-					diagnostics = { globals = { "vim" } },
-					completion = {
-						callSnippet = "Replace"
-					},
-				}
-			}
-		},
-		opts
-	)
+  -- override lua_ls
+  local lua_ls_opts = lsputils.merge(
+    {
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+            path = vim.split(package.path, ";", { plain = true }),
+          },
+          workspace = {
+            maxPreload = 100000,
+            preloadFileSize = 1000,
+          },
+          diagnostics = {
+            globals = {
+              "vim",
+              "ngx"
+            }
+          },
+          completion = {
+            callSnippet = "Replace"
+          },
+        }
+      }
+    },
+    opts
+  )
 
-	-- enable lua_ls
-	require("lspconfig").lua_ls.setup(lua_ls_opts)
+  -- enable lua_ls
+  require("lspconfig").lua_ls.setup(lua_ls_opts)
 end
 
 return M
