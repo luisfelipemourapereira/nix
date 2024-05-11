3747402c053a15d1fc03f243075668cbbc2d0b61
luis <ldesiqueira@pinger.com>
2024-05-11 14:31:58 -0300

    lockdown the flake dependencies in a strict way

--- flake.nix
+++ flake.nix
@@ -8,43 +8,47 @@
 
   inputs = {
     dream2nix = {
-      url = github:nix-community/dream2nix;
-      rev = "34a80ab215f1f24068ea9c76f3a7e5bc19478653";
+      url = github:nix-community/dream2nix?rev=34a80ab215f1f24068ea9c76f3a7e5bc19478653;
     };
     nixpkgs = {
-      url = github:NixOS/nixpkgs;
-      rev = "6c8644fc37b6e141cbfa6c7dc8d98846c4ff0c2e";
+      url = github:NixOS/nixpkgs?rev=6c8644fc37b6e141cbfa6c7dc8d98846c4ff0c2e;
     };
     home-manager = {
-      url = github:drzln/home-manager;
-      rev = "b372d7f8d5518aaba8a4058a453957460481afbc";
+      url = github:drzln/home-manager?rev=b372d7f8d5518aaba8a4058a453957460481afbc;
     };
     flake-utils = {
-      url = github:numtide/flake-utils;
-      rev = "3db36a8b464d0c4532ba1c7dda728f4576d6d073";
+      url = github:numtide/flake-utils?rev=3db36a8b464d0c4532ba1c7dda728f4576d6d073;
     };
     nix-darwin = {
-      url = github:LnL7/nix-darwin?branch=master;
-      rev = "252541bd05a7f55f3704a3d014ad1badc1e3360d";
+      url = github:LnL7/nix-darwin?rev=252541bd05a7f55f3704a3d014ad1badc1e3360d;
     };
     pythonix = {
-      url = github:Mic92/pythonix;
-      rev = "fbc84900b5dcdde558c68d98ab746d76e7884ded";
+      url = github:Mic92/pythonix?rev=fbc84900b5dcdde558c68d98ab746d76e7884ded;
     };
-    sops-nix.url = github:Mic92/sops-nix;
-    nix-funcs.url = github:t3rro/nix-funcs;
+    sops-nix = {
+      url = github:Mic92/sops-nix?rev=c5dab21d8706afc7ceb05c23d4244dcb48d6aade;
+    };
+    nix-funcs = {
+      url = github:t3rro/nix-funcs?rev=1a55a1a80e6e83ec21e5e8e0722a667c42160af6;
+    };
     bundix = {
-      url = github:nix-community/bundix?branch=master;
+      url = github:nix-community/bundix?rev=3d7820efdd77281234182a9b813c2895ef49ae1f;
       flake = false;
     };
-    hydra.url = github:NixOS/hydra?branch=master;
-    arion.url = github:hercules-ci/arion?branch=master;
-    stitches.url = github:drzln/stitches?ref=main;
+    hydra = {
+      url = github:NixOS/hydra?rev=998df1657e7e9bd3c2d54f8106eae5a325e17e02;
+    };
+    arion = {
+      url = github:hercules-ci/arion?rev=09ef2d13771ec1309536bbf97720767f90a5afa7;
+    };
+    # stitches.url = github:drzln/stitches?ref=main;
     nixt = {
-      url = github:nix-community/nixt?branch=master;
+      url = github:nix-community/nixt?rev=02a1f3c32b01ac0c097d6d67f6f5836277d3bcdc;
       flake = false;
     };
-    nur.url = github:nix-community/nur;
+    nur = {
+      url = github:nix-community/nur?rev=d2d70316f27384cf53e0f3c6cf2fd73e4744555a;
+    };
   };
 
   # end inputs
@@ -61,7 +65,7 @@
     , nix-funcs
     , pythonix
     , sops-nix
-    , stitches
+    # , stitches
     , nixpkgs
     , bundix
     , hydra
@@ -224,11 +228,11 @@
 
             # TODO: module to add stitches, consider
             # TODO: making more concise
-            cidStitchesMod = { ... }: {
-              environment.systemPackages = [
-                stitches.packages.aarch64-darwin.default
-              ];
-            };
+            # cidStitchesMod = { ... }: {
+            #   environment.systemPackages = [
+            #     stitches.packages.aarch64-darwin.default
+            #   ];
+            # };
 
             # work macos laptop
             cid = nix-darwin.lib.darwinSystem {
